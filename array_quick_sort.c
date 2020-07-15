259619e21cadbec7a1317e952a421bad89b570b1
akash-ranjan8 <56363385+akash-ranjan8@users.noreply.github.com>
2020-07-15 23:35:21 +0530

    Add files via upload

---
+++ array_quick_sort.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+int parition(int n,int a[n],int l,int h)
+{ int pivot;
+  pivot=a[l];
+  int i,j,t;
+  i=l;
+  j=h;
+  while(i<j)
+  { do
+    { ++i;
+    }while(a[i]<=pivot);
+    do
+    { --j;
+    }while(a[j]>pivot);
+   }
+  if(i<j)
+  { t=a[i];
+    a[i]=a[j];
+    a[j]=t;
+  }
+  t=a[l];
+  a[l]=a[j];
+  a[j]=t;
+  return j;
+}
+void quick_sort(int n,int a[n],int l,int h)
+{ int j;
+  if(l<h)
+  { j=parition(n,a,l,h);
+    quick_sort(n,a,l,j);
+    quick_sort(n,a,j+1,h);
+  }
+}
+void main()
+{ int n,i,l,h;
+  printf("enter the size of array\n");
+  scanf("%d",&n);
+  int a[n];
+  l=0;
+  h=n-1;
+  printf("enter the array elements\n");
+  for(i=0;i<n;++i)
+  { scanf("%d",&a[i]);
+  }
+  quick_sort(n,a,l,h);
+  printf("sorted array\n");
+  for(i=0;i<n;++i)
+  { printf("%d\t",a[i]);
+  }
+}
