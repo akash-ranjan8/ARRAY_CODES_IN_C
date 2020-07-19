f088c34827782efc6fad5af4a94285cbb94600e5
akash-ranjan8 <56363385+akash-ranjan8@users.noreply.github.com>
2020-07-19 13:03:44 +0530

    Add files via upload

---
+++ 2D_matrix_upper_lower_diagonal.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+int main()
+{ int n,i,j;
+  printf("enter the size of matrix\n");
+  scanf("%d",&n);
+  printf("enter the elements of matrix\n");
+  int a[n][n];
+  for(i=0;i<n;++i)
+  { for(j=0;j<n;++j)
+    { scanf("%d",&a[i][j]);
+    }
+  }
+//LOWER TRIANGLE
+  for(i=0;i<n;++i)
+  { printf("\n");
+    for(j=0;j<n;++j)
+    { if(i==j || i>j)
+      { printf("%d\t",a[i][j]);
+      }
+    }
+  }
+//UPPER TRIANGLE
+  for(i=0;i<n;++i)
+  { printf("\n");
+    for(j=0;j<n;++j)
+    { if(i==j || j>i)
+      { printf("%d\t",a[i][j]);
+      }
+    }
+  }
+//OTHER DIAGONAL ELEMENT
+  for(i=0;i<n;++i)
+  { printf("\n");
+    for(j=0;j<n;++j)
+    { if((i+j)%2==0)
+      { printf("%d\t",a[i][j]);
+      }
+    }
+   }
+   return 0;
+}
+   
