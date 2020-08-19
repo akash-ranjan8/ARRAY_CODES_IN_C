9603ab43224ac05424d7e0dae1e060dfc25c4dd5
akash-ranjan8 <56363385+akash-ranjan8@users.noreply.github.com>
2020-08-19 17:28:35 +0530

    Add files via upload

---
+++ array_heap_sort.c
@@ -0,0 +1,63 @@
+    #include<stdio.h> 
+    #include<time.h> 
+    int temp;  
+      
+    void heapify(int arr[], int size, int i)  
+    {  
+    int largest = i;    
+    int left = 2*i + 1;    
+    int right = 2*i + 2;    
+      
+    if (left < size && arr[left] >arr[largest])  
+    largest = left;  
+      
+    if (right < size && arr[right] > arr[largest])  
+    largest = right;  
+      
+    if (largest != i)  
+    {  
+    temp = arr[i];  
+        arr[i]= arr[largest];   
+        arr[largest] = temp;  
+    heapify(arr, size, largest);  
+    }  
+    }  
+      
+    void heapSort(int arr[], int size)  
+    {  
+    int i;  
+    for (i = size / 2 - 1; i >= 0; i--)  
+    heapify(arr, size, i);  
+    for (i=size-1; i>=0; i--)  
+    {  
+    temp = arr[0];  
+        arr[0]= arr[i];   
+        arr[i] = temp;  
+    heapify(arr, i, 0);  
+    }  
+    }  
+      
+    void main()  
+    {  
+    int n;
+    printf("enter the size of array\n");
+    scanf("%d",&n);
+    int a[n];  
+    int i;
+    printf("enter the elements\n");
+    for(i=0;i<n;++i)
+    { scanf("%d",&a[i]);
+    }
+    clock_t t;
+    t=clock();  
+    int size = sizeof(a)/sizeof(a[0]); 
+      
+    heapSort(a, size);  
+      
+    printf("printing sorted elements\n");  
+    for (i=0; i<size; ++i)  
+    printf("%d\n",a[i]);
+    t=clock()-t;
+    double time_taken=((double)t)/CLOCKS_PER_SEC;
+    printf("time taken : %f\n",time_taken);  
+    }  
